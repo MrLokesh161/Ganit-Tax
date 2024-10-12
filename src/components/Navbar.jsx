9b9bbb0074a7a4912088477414108f994b29b871
MrLokesh161 <lokesh07084@gmail.com>
2024-10-13 00:00:13 +0530

    updated responsive home

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,13 +6,9 @@ import { faFacebook, faInstagram, faLinkedin, faWhatsapp } from '@fortawesome/fr
 const Navbar = () => {
   return (
     <header>
-      {/* Combined Top Bar and Navigation */}
       <div className="bg-orange-300 pb-[3%] mb-[-4%] z-20 ">
-        {/* Top Bar */}
         <div className="text-gray-900 px-4 py-2 flex flex-col md:flex-row justify-between items-center w-[90%] mx-auto">
-          {/* Top Section: Phone, Location, and Email */}
           <div className="flex flex-col md:flex-row items-center justify-between space-y-2 md:space-y-0 md:space-x-8 text-sm w-full">
-            {/* Contact Info */}
             <div className="flex items-center space-x-2">
               <span>
                 <FontAwesomeIcon icon={faPhoneAlt} className="text-gray-900" /> +1 844-636-7692
@@ -22,13 +18,11 @@ const Navbar = () => {
               </span>
             </div>
 
-            {/* Email */}
             <span className="flex items-center">
               <FontAwesomeIcon icon={faSearch} className="text-gray-900" /> hello@fint.com
             </span>
           </div>
 
-          {/* Social Icons */}
           <div className="flex items-center space-x-4 mt-2 md:mt-0">
             <h5>//</h5>
             <a href="https://facebook.com" className="text-gray-900" target="_blank" rel="noopener noreferrer">
